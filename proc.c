b03b4b06825e0e8ec34dc4bea3e9ee597f016f47
DM <mcnally@udel.edu>
2021-10-27 10:53:36 -0400

    Added csrp() attempt

--- proc.c
+++ proc.c
@@ -336,6 +336,7 @@ scheduler(void)
       if(p->state != RUNNABLE)
         continue;
       //cprintf("process [%s:%d] is running\n",p->name, p->pid);
+      // TODO: UNCOMMENT ^
       
       // Switch to chosen process.  It is the process's job
       // to release ptable.lock and then reacquire it
@@ -355,6 +356,27 @@ scheduler(void)
 
   }
 }
+
+// Current, Running, and Sleeping Processes
+// Added for 361 project.
+// Found in proc.c
+int csrp(void) {
+  struct proc *p;
+  struct cpu *c = mycpu();
+  c->proc = 0;
+
+  acquire(&ptable.lock);
+  for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+      if(p->state == RUNNABLE) {
+        cprintf("process [%s:%d] is running\n",p->name, p->pid);
+      }
+      if(p->state == SLEEPING) {
+        cprintf("process [%s:%d] is sleeping\n",p->name, p->pid);
+      }
+  }
+  release(&ptable.lock);
+}
+
 
 // Enter scheduler.  Must hold only ptable.lock
 // and have changed proc->state. Saves and restores
